719e24de775fe08a6fc7f4c1a90a639bc9d34599
Junhyeong Jo <whwnsgud13@hanyang.ac.kr>
2022-02-02 23:28:54 +0900

    MethodFile.cs

---
+++ MethodFile.cs
@@ -0,0 +1,32 @@
+// Method(메서드) 문에 대해서 알아보자! 
+
+// using System;
+
+// namespace TestFile
+// {
+//     class Calculator
+//     {
+//         public static int Plus(int a, int b)
+//         {
+//             return a + b;
+//         }
+
+//         public static int Minus(int a, int b)
+//         {
+//             return a - b;
+//         }
+//     }
+
+//     class MainApp
+//     {
+//         public static void Main()
+//         {
+//             int result = Calculator.Plus(3,4);
+//             Console.WriteLine(result);
+
+//             result = Calculator.Minus(10, 4);
+//             Console.WriteLine(result);
+//         }
+//     }
+// }
+
