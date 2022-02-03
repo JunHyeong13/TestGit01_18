1a74bcd46815d9d6e5b11fecef9a51d33354f9f5
Junhyeong Jo <whwnsgud13@hanyang.ac.kr>
2022-02-03 17:41:19 +0900

    Overloading.cs

--- Overloading.cs
+++ Overloading.cs
@@ -1,37 +1,37 @@
-using System;
+// using System;
 
-namespace Overloading
-{
-    class MainApp
-    {
-        static int Plus(int a, int b)
-        {
-            Console.WriteLine("Calling int Plus(int, int)...");
-            return a + b;
-        }
-        static int Plus(int a, int b, int c)
-        {
-            Console.WriteLine("Calling int Plus(int, int, int).. ");
-            return a + b + c;
-        }
+// namespace Overloading
+// {
+//     class MainApp
+//     {
+//         static int Plus(int a, int b)
+//         {
+//             Console.WriteLine("Calling int Plus(int, int)...");
+//             return a + b;
+//         }
+//         static int Plus(int a, int b, int c)
+//         {
+//             Console.WriteLine("Calling int Plus(int, int, int).. ");
+//             return a + b + c;
+//         }
 
-        static double Plus(double a, double b)
-        {
-            Console.WriteLine("Calling double Plus(double, double)...");
-            return a + b;
-        }
+//         static double Plus(double a, double b)
+//         {
+//             Console.WriteLine("Calling double Plus(double, double)...");
+//             return a + b;
+//         }
 
-        static double Plus(int a, double b)
-        {
-            Console.WriteLine("Calling double Plus(int, double)");
-            return a + b;
-        }
-        static void Main(string[]args)
-        {
-            Console.WriteLine(Plus(1,2)); // 3
-            Console.WriteLine(Plus(1,2,3)); // 6
-            Console.WriteLine(Plus(1.0,2.0)); // 3.0
-            Console.WriteLine(Plus(1,2.4)); // 3.4
-        }
-    }
-}
+//         static double Plus(int a, double b)
+//         {
+//             Console.WriteLine("Calling double Plus(int, double)");
+//             return a + b;
+//         }
+//         static void Main(string[]args)
+//         {
+//             Console.WriteLine(Plus(1,2)); // 3
+//             Console.WriteLine(Plus(1,2,3)); // 6
+//             Console.WriteLine(Plus(1.0,2.0)); // 3.0
+//             Console.WriteLine(Plus(1,2.4)); // 3.4
+//         }
+//     }
+// }
