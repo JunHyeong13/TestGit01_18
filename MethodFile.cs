229d68b3a43b7a505a868db32ea0b997c98c0f06
JunHyeong13 <whwnsgud13@hanyang.ac.kr>
2022-02-06 17:29:11 +0900

    NewLocalFile

--- MethodFile.cs
+++ MethodFile.cs
@@ -1,32 +1,31 @@
-// Method(메서드) 문에 대해서 알아보자! 
+//Method(메서드) 문에 대해서 알아보자! 
 
-// using System;
+using System;
 
-// namespace TestFile
-// {
-//     class Calculator
-//     {
-//         public static int Plus(int a, int b)
-//         {
-//             return a + b;
-//         }
+namespace TestFile
+{
+    class Calculator
+    {
+        public static int Plus(int a, int b)
+        {
+            return a + b;
+        }
 
-//         public static int Minus(int a, int b)
-//         {
-//             return a - b;
-//         }
-//     }
+        public static int Minus(int a, int b)
+        {
+            return a - b;
+        }
+    }
 
-//     class MainApp
-//     {
-//         public static void Main()
-//         {
-//             int result = Calculator.Plus(3,4);
-//             Console.WriteLine(result);
+    class MainApp
+    {
+        public static void Main()
+        {
+            int result = Calculator.Plus(3,4);
+            Console.WriteLine(result);
 
-//             result = Calculator.Minus(10, 4);
-//             Console.WriteLine(result);
-//         }
-//     }
-// }
-
+            result = Calculator.Minus(10, 4);
+            Console.WriteLine(result);
+        }
+    }
+}
